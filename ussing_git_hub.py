e7b830138fbb15ddcdfbe2b0e9fbcef3dee758cd
Scholar <scholar@paradigm.local>
2023-08-31 12:22:01 -0600

    changed var to character

--- ussing_git_hub.py
+++ ussing_git_hub.py
@@ -1,7 +1,7 @@
-my_character={
+character={
 'name':"anya forger",
 'age':4,
 'male': False,
 'foods': ["penuts", "loids food", "pizza", "chiken", "i dont know"],
 }
-print (my_character)
+print (character)
