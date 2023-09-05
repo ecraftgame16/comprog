14d07e7a3e8968468f1878dba61f7454a907c2dd
Scholar <scholar@paradigm.local>
2023-09-05 12:38:10 -0600

    help me

---
+++ variabls.py
@@ -0,0 +1,16 @@
+#variabls
+name = "Etienne"
+
+bts = 100
+
+good = True
+
+my_classes = ["senimar", "computer programing", "math"]
+
+#function
+import random 
+def add(num1,num2):
+    print(num1+num2)
+
+add(num1=random.randint(1,100),num2=random.randint(1,100))
+    
