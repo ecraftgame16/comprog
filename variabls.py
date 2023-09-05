da2c1101b4f65de0a41246795eb8c1d2224feebe
Scholar <scholar@paradigm.local>
2023-09-05 13:09:52 -0600

    I hate this

--- variabls.py
+++ variabls.py
@@ -14,3 +14,5 @@ def add(num1,num2):
 
 add(num1=random.randint(1,100),num2=random.randint(1,100))
     
+
+print(f"Hi, my name is {name}. I just scored {bts} on my last test. My classes today are {my_classes}")
