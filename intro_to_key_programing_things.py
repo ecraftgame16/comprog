0f5c369ce82115d4d5b8a08e4f11b37610380611
Scholar <scholar@paradigm.local>
2023-09-08 11:15:31 -0600

    1/2 beacuse i am stupid

--- intro_to_key_programing_things.py
+++ intro_to_key_programing_things.py
@@ -54,16 +54,51 @@ if x and not y:
     pass
 #etc
 
-print(3/5)#int div
-print(3.0/5.0) #float div
+#order of operations (pemmdas) paretheses exponest multipilcation modulud division addition subtraction
 
+b = 10 #initislation statment
+u = b + 10 #exsprestion beacuse it can change
+
+print (3 // 5)#int div
+print (3 / 5) #float div
+
+#function
 def function():
     print ("this is a function")
+
+function()
+
 #readablity
 thisisnotreadable=True
 this_is_readable = True
 
-#bultin function
-x = "this will be a list"
-A = list(x)
-print (A)
+#resuibility
+def add(num1, num2):
+    print(num1 + num2)
+    #can reuse this function 
+
+#modularity reusibility and limits duplication
+
+#usser defined function
+ussernum1 = int(input("give me a number"))
+ussernum2 = int(input("give me another one"))
+
+def usser_add(ussernum1, ussernum2): #thease () are the paramiters
+    print(ussernum1 + ussernum2) #also () are the paramiters
+usser_add(ussernum1, ussernum2) #the () are the agurment
+
+#bult in function
+my_name = "tacocat"
+words = my_name.split("o")
+print (words)
+
+#return
+def subtract(num3,num4):
+    return num3 - num4
+
+print(subtract(4,2))
+#or
+diffrence = subtract (5,3)
+
+#simple data type charater string integer float double boolean
+#complex data type enumeration, array, list, object
