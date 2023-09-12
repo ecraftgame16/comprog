cb108a73bbd2b760e83e3bbed54f0d5e308718d0
Scholar <scholar@paradigm.local>
2023-09-12 12:57:20 -0600

    I MADE A SIMPLE CALCULATOR

---
+++ simple_calculator.py
@@ -0,0 +1,36 @@
+num1 = int(input("give me a number. "))
+num2 = int(input("give me another number. "))
+operator = int(input("""and finaly whitch operator do you want? 
+          1 addiotion
+          2 subtraction
+          3 multipication
+          4 division. """))
+
+#the individual functions
+def addiotion(num1,num2):
+    print(f"the answer is", num1 + num2)
+
+def subtraction(num1, num2):
+    print(f"the answer is", num1-num2)
+
+def multiplicaton(num1,num2):
+    print(f"the answer is", num1*num2)
+
+def divison(num1,num2):
+    print(f"the answer is", num1/num2)
+
+#calling the functons
+if operator == 1:
+    addiotion (num1,num2)
+
+elif operator  == 2:
+    subtraction (num1,num2)
+
+elif operator == 3:
+    multiplicaton(num1,num2)
+
+elif operator == 4:
+    divison (num1,num2)
+
+else:
+    print("please enter a nimber 1-4 depending on the operator")
