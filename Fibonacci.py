e522bcf121fb150a13ca2a217b9a4fafbd7a6a9a
Scholar <scholar@paradigm.local>
2023-10-03 12:47:14 -0600

    the importance of flow charts

---
+++ Fibonacci.py
@@ -0,0 +1,11 @@
+sequence = int(input("how many in the squence do you want? "))
+sequence = sequence - 2
+origion =2 
+current = 1
+print(1)
+print(1)
+for x in range(sequence):
+    print (origion)
+    old_origion = origion
+    origion += current
+    current = old_origion
