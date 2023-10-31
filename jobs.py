267f66dabdc7a9f7ffa748bdf4e8403a6deded30
Scholar <scholar@paradigm.local>
2023-10-31 12:47:44 -0600

    jobs

---
+++ jobs.py
@@ -0,0 +1,27 @@
+computer_programing_team = {
+    "team_leader": {
+        'name': 'Jim',
+        "duties": ["supervise", "assist", "advocate"]
+    },
+    "analyst": {
+        "name": "Oscar",
+        "duties": ["analyze", "examine", "collect"]
+    },
+    "senior_devoloper":{
+        'name': "Dwight",
+        "duties": ["train", "assist", "develop"]
+    }, 
+    "junior_devoper" : {
+        "name": "ryan",
+        "duties" : ["learn","develop"]
+    },
+    "client":{
+        'name':'Jan',
+        "duties": ["propose", "critique", "request"]
+    },
+    "product_maneger": {
+        "name" : "michael",
+        "duties": ["manage","mediate", "advocate"]
+
+    }
+}
