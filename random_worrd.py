042ca76ca510d03204fa104a27b45d45a25a7747
Scholar <scholar@paradigm.local>
2023-11-07 13:41:08 -0700

    I hate life

---
+++ random_worrd.py
@@ -0,0 +1,10 @@
+import random
+i = random.randint(0,267751)
+with open('words.txt', 'r') as f:
+    line = f.read()
+    words = line.split()
+    wrods_list=[]
+    for wordle in words:
+        wrods_list.append
+word = wrods_list[i]
+print(word)
