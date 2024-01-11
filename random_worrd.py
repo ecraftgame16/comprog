1a824eb61b96fe6b26370ea7020d58a633ac5d5a
Scholar <scholar@paradigm.local>
2024-01-11 12:43:27 -0700

    the great reset

--- random_worrd.py
+++
@@ -1,6 +0,0 @@
-import random
-words_list=[]
-f =open("sowpods.txt","r")
-for x in f:
-    words_list.append(x)
-print (random.choice(words_list))
--- ussing_git_hub.py
+++
@@ -1,7 +0,0 @@
-character={
-'name':"anya forger",
-'age':4,
-'male': False,
-'foods': ["penuts", "loids food", "pizza", "chiken", "i dont know"],
-}
-print (character)
