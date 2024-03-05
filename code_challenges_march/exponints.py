6518981652d046d5599a62dc7ca7a0a66d758a2f
etienne <etiennehanzon1@gmail.com>
2024-03-05 12:51:42 -0700

    test name

---
+++ code_challenges_march/exponints.py
@@ -0,0 +1,12 @@
+import math
+
+def exponent(base, power):
+    if power == 0:
+        return 1
+    elif power < 0:
+        return "i dont get payed enough for this"
+    return base ** power
+
+base = int(input ("what is the base"))
+power = int(input("what is the power"))
+print(exponent(base, power))
